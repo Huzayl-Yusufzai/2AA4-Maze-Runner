a4cc5508c8dd924c983952514ab1ada594d1fc58
Huzayl Yusuf-zai <huzayl.yusufzai@gmail.com>
2025-01-24 23:59:34 -0500

    MVP

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -32,19 +32,13 @@ public class Main {
             logger.info("**** Reading the maze from file {}", filePath);
 
             try {
-                logger.info("**** Reading the maze from file {}", filePath);
+
                 BufferedReader reader = new BufferedReader(new FileReader(filePath));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    for (int idx = 0; idx < line.length(); idx++) {
-                        if (line.charAt(idx) == '#') {
-                            System.out.print("WALL ");
-                        } else if (line.charAt(idx) == ' ') {
-                            System.out.print("PASS ");
-                        }
-                    }
-                    System.out.print(System.lineSeparator());
-                }
+                String line= "";
+                Map map = new Map(reader, line, filePath);
+                logger.info("**** Computing path");
+                map.initiateCheck();
+                
             } catch(Exception e) {
                 System.err.println("/!\\ An error has occured /!\\");
             }
@@ -53,8 +47,165 @@ public class Main {
             System.out.println("Usage: <File call> -i <path_to_maze_file>");
             return;
         }
-        logger.info("**** Computing path");
-        logger.warn("Pathfinding logic not implemented yet.");
         logger.info("** End of Maze Runner");
+    }
+}
+
+class Map {
+    private boolean[][] mapValues;
+    private int currentRow;
+    private int currentCol;
+
+    public Map(BufferedReader reader, String line, String filePath) {
+        try {
+            int row = 0;
+            while ((line = reader.readLine()) != null) {
+                for (int idx = 0; idx < line.length(); idx++) {
+                    if (line.charAt(idx) == '#') {
+                        mapValues[idx][row]= false;
+                    } else if (line.charAt(idx) == ' ') {
+                        mapValues[idx][row]= true;
+                    }
+                }
+                System.out.print(System.lineSeparator());
+                row++;
+            }
+        } catch (Exception e){
+            System.err.println("/!\\ An error has occured /!\\");
+        }
+    }
+
+    public void catchStart(){
+        for (int i = 0; i < mapValues.length; i++) {
+            if(mapValues[i][0] == true){
+                currentRow = i;
+                break;
+            }
+        }
+        currentCol = 0;
+    }
+
+    public void initiateCheck(){
+        Marker marker = new Marker(currentRow, currentCol, mapValues);
+        marker.rightHandRule();
+        System.out.println(marker.returnPath());
+    }
+    
+}
+class Marker {
+    private String path = "";
+    private boolean[][] mapValues;
+    private int currentRow;
+    private int currentCol;
+    private String direction = "W";
+
+    public Marker(int row, int col, boolean[][] mapValues) {
+        currentRow = row;
+        currentCol = col;
+        this.mapValues = mapValues;
+    }
+
+    public void rightHandRule(){
+       while (mapValues[currentRow][currentCol]!= null){
+            if (direction.equals("E")){
+                if (mapValues[currentRow-1][currentCol] == false){
+                    if (mapValues[currentRow][currentCol+1] == false){
+                        if (mapValues[currentRow+1][currentCol] == false){
+                            direction = "W";
+                            addToPath("LLLF");
+                            currentCol++;   
+                        } else {
+                            direction = "N";
+                            addToPath("LF");
+                            currentCol++;     
+                        }
+                    } else {
+                        direction = "E";
+                        addToPath("F");
+                        currentCol++;    
+                    }
+                } else {
+                    direction = "S";
+                    addToPath("RF");
+                    currentRow--;
+                }
+            }
+            if (direction.equals("N")){
+                if (mapValues[currentRow][currentCol+1] == false){
+                    if (mapValues[currentRow+1][currentCol] == false){
+                        if (mapValues[currentRow][currentCol-1] == false){
+                            direction = "S";
+                            addToPath("LLLF");
+                            currentRow--;   
+                        } else {
+                            direction = "W";
+                            addToPath("LF");
+                            currentCol--;     
+                        }
+                    } else {
+                        direction = "N";
+                        addToPath("F");
+                        currentRow++;    
+                    }
+                } else {
+                    direction = "E";
+                    addToPath("RF");
+                    currentCol++;
+                }
+            }
+            if (direction.equals("W")){
+                if (mapValues[currentRow+1][currentCol] == false){
+                    if (mapValues[currentRow][currentCol-1] == false){
+                        if (mapValues[currentRow-1][currentCol] == false){
+                            direction = "E";
+                            addToPath("LLLF");
+                            currentCol--;   
+                        } else {
+                            direction = "S";
+                            addToPath("LF");
+                            currentCol--;     
+                        }
+                    } else {
+                        direction = "W";
+                        addToPath("F");
+                        currentCol--;    
+                    }
+                } else {
+                    direction = "N";
+                    addToPath("RF");
+                    currentRow++;
+                }
+            if (direction.equals("S")){
+                if (mapValues[currentRow][currentCol-1] == false){
+                    if (mapValues[currentRow-1][currentCol] == false){
+                        if (mapValues[currentRow][currentCol+1] == false){
+                            direction = "N";
+                            addToPath("LLLF");
+                            currentRow++;   
+                        } else {
+                            direction = "W";
+                            addToPath("LF");
+                            currentCol++;     
+                        }
+                    } else {
+                        direction = "S";
+                        addToPath("F");
+                        currentRow--;    
+                    }
+                } else {
+                    direction = "W";
+                    addToPath("RF");
+                    currentCol--;
+                }
+            }
+       }
+    }
+
+    public void addToPath(String direction){
+        path = path.concat(direction);
+    }
+
+    public String returnPath(){
+        return path;
     }
 }
