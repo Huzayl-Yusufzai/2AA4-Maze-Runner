1bce28722c804a7143c664bf28e880e38b69631a
Huzayl Yusuf-zai <huzayl.yusufzai@gmail.com>
2025-02-01 01:48:23 -0500

    Added check path feature, fixed issue with rectangular maze, and added comments for readbility. Closes #10, Closes #13

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -2,7 +2,8 @@ package ca.mcmaster.se2aa4.mazerunner;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -18,6 +19,7 @@ public class Main {
     public static void main(String[] args) {
         logger.info("** Starting Maze Runner");
 
+        //Add flag options to input
         Options options = new Options();
         options.addOption("i", "input", true, "Maze input path");
         options.addOption("p", "input", true, "Proposed Maze Solution");
@@ -26,13 +28,13 @@ public class Main {
         CommandLine cmd;
         try {
             cmd = parser.parse(options, args);
-            if (cmd.hasOption("i") && cmd.hasOption("p")){
+            if (cmd.hasOption("i") && cmd.hasOption("p")){ //If user path needs to be checked
 
                 String filePath = cmd.getOptionValue("i");
                 logger.info("**** Reading the maze from file {}", filePath);
 
                 String userPath = cmd.getOptionValue("p");
-                logger.info("**** Useing '{}' as  input path", userPath);
+                logger.info("**** Using '{}' as  input path", userPath);
 
                 try {
 
@@ -42,13 +44,13 @@ public class Main {
                     logger.info("**** Simulating Outcome");
                     map.catchStartnEnd();
                     map.createMarker();
-                    System.out.println();
+                    System.out.println("Input path is " + map.initiateCheck(userPath));
 
                 } catch (Exception e) {
                     logger.error("/!\\ An error has occurred: {}", e.getMessage(), e);
                 }
 
-            } else {
+            } else { //If path needs to be generated
                 if (!cmd.hasOption("i")) {
                     logger.error("-i flag required for input path");
                     System.out.println("Usage: <File call> -i <path_to_maze_file>");
@@ -66,7 +68,8 @@ public class Main {
                     logger.info("**** Computing path");
                     map.catchStartnEnd();
                     map.createMarker();
-                    System.out.println("East Entry: " + map.initiateCheck());
+                    System.out.println("West Entry:");
+                    map.initiateSearch();
 
                 } catch (Exception e) {
                     logger.error("/!\\ An error has occurred: {}", e.getMessage(), e);
@@ -81,7 +84,7 @@ public class Main {
     }
 }
 
-class Map {
+class Map { //Class to deal woth map data
 
     private boolean[][] mapValues;
     private int currentRow;
@@ -91,38 +94,34 @@ class Map {
     private int endCol;
 
     public Map(BufferedReader reader, String line, String filePath) {
-        try {
-            int row = 0;
+        try { //Text file to boolean array
+            List<String> lines = new ArrayList<>();
             int cols = 0;
 
-            reader.mark(10000);
-
-            while ((line = reader.readLine()) != null) { //Allocating space for array
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
                 cols = Math.max(cols, line.length());
-                row++;
             }
 
-            mapValues = new boolean[cols][row];
+            int rows = lines.size();
+            mapValues = new boolean[rows][cols];
 
-            reader.reset();
-            row = 0;
-
-            while ((line = reader.readLine()) != null) { //Populating with Boolean values according to text file
-                for (int idx = 0; idx < line.length(); idx++) {
-                    if (line.charAt(idx) == '#') {
-                        mapValues[row][idx] = false; //Wall
-                    } else if (line.charAt(idx) == ' ') {
-                        mapValues[row][idx] = true; //Path
+            for (int row = 0; row < rows; row++) {
+                line = lines.get(row);
+                for (int col = 0; col < line.length(); col++) {
+                    if (line.charAt(col) == '#') {
+                        mapValues[row][col] = false; // WALL
+                    } else {
+                        mapValues[row][col] = true; // PATH
                     }
                 }
-                row++;
             }
         } catch (Exception e) {
             System.out.println(e);
         }
     }
 
-    public void catchStartnEnd() {
+    public void catchStartnEnd() { //Finds exit and enterences on each side of the map
         for (int i = 0; i < mapValues.length; i++) {
             if (mapValues[i][0] == true) {
                 currentRow = i;
@@ -131,33 +130,40 @@ class Map {
             }
         }
 
-        for (int i = 0; i < mapValues.length; i++) {
-            if (mapValues[i][mapValues.length-1] == true) {
+        for (int i = 0; i < mapValues.length; i++) { 
+            if (mapValues[i][mapValues[i].length-1] == true) {
                 endRow = i;
-                endCol = mapValues.length-1;
+                endCol = mapValues[i].length-1;
                 return;
             }
         }
     }
 
-    public void createMarker(){
+    public void createMarker(){ //Creates a marker for the map
         marker = new Marker(currentRow, currentCol, mapValues, endRow, endCol);
     }
 
-    public String initiateCheck() {
+
+    public void initiateSearch() { //States the path if it needs to be determined
         marker.rightHandRule();
-        return marker.returnPath();
+        System.out.println("    Canonical: " + marker.returnCanonicalPath());
+        System.out.println("    Factorized: " + marker.returnFactorizedPath());
+    }
+
+    public String initiateCheck(String userPath){ //Starts the verification if path needs to be checked
+        return marker.checkPath(userPath);
     }
 
 }
 
-class Marker {
+class Marker { //Marker class which walks through the maze
 
     private String path = "";
     private boolean[][] mapValues;
     private int currentRow;
     private int currentCol;
-
+    private int eastRow;
+    private int eastCol;
     private int endRow;
     private int endCol;
     private String direction = "E";
@@ -172,7 +178,6 @@ class Marker {
 
     public void rightHandRule() {
         while (true) {
-            //System.out.println("Current Position: (" + currentRow + ", " + currentCol + ") | Direction: " + direction);
             switch (direction) {
                 case "E": //When Facing East
                     if (isValid(currentRow + 1, currentCol)) { //Checking SOUTH
@@ -246,7 +251,7 @@ class Marker {
                         direction = "S";
                         addToPath("LF");
                         currentRow++;
-                    } else { //GO WEST
+                    } else { //GO EAST
                         direction = "E";
                         addToPath("RRF");
                         currentCol++;
@@ -255,29 +260,32 @@ class Marker {
 
             
             }
-
             if (endCol == currentCol && endRow == currentRow){
                 break;
             }
         }
     }
 
-    private boolean isValid(int row, int col) {
+    private boolean isValid(int row, int col) { //Checks if potion is valid
         return row >= 0 && row < mapValues.length
                 && col >= 0 && col < mapValues[0].length
                 && mapValues[row][col];
     }
 
-    public void addToPath(String direction) {
+    public void addToPath(String direction) { //Adds steps to path
         path = path.concat(direction);
     }
 
-    public String returnPath() {
+    public String returnCanonicalPath() { //Returns canonized path
+        return path;
+    }
+
+    public String returnFactorizedPath() { //Returns factorized path
         path = factorize();
         return path;
     }
 
-    public String factorize(){
+    public String factorize(){ //Factorizes the path
         char[] splitPath = path.toCharArray();
         char previous = splitPath[0];
         String newFactorized="";
@@ -286,8 +294,7 @@ class Marker {
 
         for (char chars: splitPath){
             if (previous !=chars){
-
-                newFactorized = (counter ==1 ) ? newFactorized.concat(String.valueOf(previous + " ")): newFactorized.concat(String.valueOf(counter)).concat(String.valueOf(previous + " "));
+                newFactorized = (counter == 1 ) ? newFactorized.concat(String.valueOf(previous + " ")): newFactorized.concat(String.valueOf(counter)).concat(String.valueOf(previous + " "));
                 previous = chars;
                 counter=1;
             } else {
@@ -295,9 +302,170 @@ class Marker {
             }
             index++;
         }
-        newFactorized = (counter ==1 ) ? newFactorized.concat(String.valueOf(previous + " ")): newFactorized.concat(String.valueOf(counter)).concat(String.valueOf(previous + " "));
+        newFactorized = (counter == 1 ) ? newFactorized.concat(String.valueOf(previous + " ")): newFactorized.concat(String.valueOf(counter)).concat(String.valueOf(previous + " "));
         return newFactorized;
     }
 
+    public String checkPath(String userInput){ //Manager method for west to east and east to west checks
+        eastCol = currentCol;
+        eastRow = currentRow;
 
+        //Start from east entrance
+        if (west2east(userInput)){
+            return "valid";
+        } else if (east2west(userInput)){
+            return "valid";
+        } else {
+            return "not valid";
+        }
+    }
+
+    public boolean west2east(String userInput){ //West to east check
+        char[] userArrInput = userInput.toCharArray();
+        direction = "E";
+        for (char chars: userArrInput){
+            switch (direction) {
+                case "E": //When Facing East
+                    if (String.valueOf(chars).equals("F")){
+                        currentCol++;
+                        if (isValid(currentRow, currentCol)){
+                            continue;
+                        } else {
+                            return false;
+                        }
+                    } else if (String.valueOf(chars).equals("R"))  {
+                        direction = "S";
+                    } else if (String.valueOf(chars).equals("L")){
+                        direction = "N";
+                    }
+                    break;
+
+                case "N": //When Facing NORTH
+                    if (String.valueOf(chars).equals("F")){
+                        currentRow--;
+                        if (isValid(currentRow, currentCol)){
+                            continue;
+                        } else {
+                            return false;
+                        }
+                    } else if (String.valueOf(chars).equals("R"))  {
+                        direction = "E";
+                    } else if (String.valueOf(chars).equals("L")){
+                        direction = "W";
+                    }
+                    break;
+
+                case "S": //When Facing SOUTH
+                    if (String.valueOf(chars).equals("F")){
+                        currentRow++;
+                        if (isValid(currentRow, currentCol)){
+                            continue;
+                        } else {
+                            return false;
+                        }
+                    } else if (String.valueOf(chars).equals("R"))  {
+                        direction = "W";
+                    } else if (String.valueOf(chars).equals("L")){
+                        direction = "E";
+                    }
+                    break;
+
+                case "W": //When Facing WEST
+                    if (String.valueOf(chars).equals("F")){
+                        currentCol--;
+                        if (isValid(currentRow, currentCol)){
+                            continue;
+                        } else {
+                            return false;
+                        }
+                    } else if (String.valueOf(chars).equals("R"))  {
+                        direction = "N";
+                    } else if (String.valueOf(chars).equals("L")){
+                        direction = "S";
+                    }
+                    break;
+            }
+        }
+        if (currentCol == endCol && currentRow == endRow){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean east2west(String userInput){ // East to west check
+        currentCol = endCol;
+        currentRow = endRow;
+
+        char[] userArrInput = userInput.toCharArray();
+        direction = "W";
+        for (char chars: userArrInput){
+            switch (direction) {
+                case "E": //When Facing East
+                    if (String.valueOf(chars).equals("F")){
+                        currentCol++;
+                        if (isValid(currentRow, currentCol)){
+                            continue;
+                        } else {
+                            return false;
+                        }
+                    } else if (String.valueOf(chars).equals("R"))  {
+                        direction = "S";
+                    } else if (String.valueOf(chars).equals("L")){
+                        direction = "N";
+                    }
+                    break;
+
+                case "N": //When Facing NORTH
+                    if (String.valueOf(chars).equals("F")){
+                        currentRow--;
+                        if (isValid(currentRow, currentCol)){
+                            continue;
+                        } else {
+                            return false;
+                        }
+                    } else if (String.valueOf(chars).equals("R"))  {
+                        direction = "E";
+                    } else if (String.valueOf(chars).equals("L")){
+                        direction = "W";
+                    }
+                    break;
+
+                case "S": //When Facing SOUTH
+                    if (String.valueOf(chars).equals("F")){
+                        currentRow++;
+                        if (isValid(currentRow, currentCol)){
+                            continue;
+                        } else {
+                            return false;
+                        }
+                    } else if (String.valueOf(chars).equals("R"))  {
+                        direction = "W";
+                    } else if (String.valueOf(chars).equals("L")){
+                        direction = "E";
+                    }
+                    break;
+
+                case "W": //When Facing WEST
+                    if (String.valueOf(chars).equals("F")){
+                        currentCol--;
+                        if (isValid(currentRow, currentCol)){
+                            continue;
+                        } else {
+                            return false;
+                        }
+                    } else if (String.valueOf(chars).equals("R"))  {
+                        direction = "N";
+                    } else if (String.valueOf(chars).equals("L")){
+                        direction = "S";
+                    }
+                    break;
+            }
+        }
+        if (currentCol == eastCol && currentRow == eastRow){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
